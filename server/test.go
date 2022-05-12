a9823782ca23a2c2f85beef41de4b6fa8001b6e1
caldev <caldart123@gmail.com>
2022-05-12 00:21:15 -0300

    test

---
+++ server/test.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"context"
+
+	"github.com/acaldo/grpc/models"
+	"github.com/acaldo/grpc/repository"
+	"github.com/acaldo/grpc/testpb"
+)
+
+type TestServer struct {
+	repo repository.Repository
+	testpb.UnimplementedTestServiceServer
+}
+
+func NewTestServer(repo repository.Repository) *TestServer {
+	return &TestServer{repo: repo}
+}
+
+func (s *TestServer) GetTest(ctx context.Context, req *testpb.GetTestRequest) (*testpb.Test, error) {
+	test, err := s.repo.GetTest(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return &testpb.Test{
+		Id:   test.Id,
+		Name: test.Name,
+	}, nil
+}
+
+func (s *TestServer) SetTest(ctx context.Context, req *testpb.Test) (*testpb.SetTestResponse, error) {
+	test := models.Test{
+		Id:   req.GetId(),
+		Name: req.GetName(),
+	}
+	err := s.repo.SetTest(ctx, &test)
+	if err != nil {
+		return nil, err
+	}
+	return &testpb.SetTestResponse{
+		Id:   test.Id,
+		Name: test.Name,
+	}, nil
+}
